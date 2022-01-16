e6dd96ad2b9767717905596e07290d70a60ba720
Glitch (glitch-hello-website) <none>
2022-01-16 16:43:56 +0000

    🐊👵 Checkpoint ./shorturl/index.html:41104452/22906 ./script.js:41104452/5254 ./index.html:41104452/1420 ./TODO.md:41104452/1154

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
-
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
+document.getElementById("myinput").onclick = function () {
+  var link = document.getElementById("linkinput").value;
+  var data = {
+    domain: "go.rudyp.me",
+    originalURL: link,
+    allowDuplicates: false,
   };
+  fetch("https://api.short.cm/links/public", {
+    method: "post",
+    headers: {
+      accept: "application/json",
+      "Content-Type": "application/json",
+      authorization: "pk_hcuntZGQkuDfjvFT",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      document.getElementById("message").innerHTML =
+        "Your short link is " + data.shortURL;
+    });
+  document.getElementById("linkinput").value = "";
+};
+if (location.protocol != "https:") {
+  location.href =
+    "https:" + window.location.href.substring(window.location.protocol.length);
 }
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
